e131ff9c821d08878715908ccdc974adcdc17fe8
Raneem <raneemalhorany174@gmail.com>
2025-10-01 00:17:51 +0300

    project

---
+++ app/src/main/java/com/example/counterapp/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.counterapp
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        val score_text = findViewById<TextView>(R.id.score_text)
+        val  addOneButton = findViewById<Button>(R.id.addOne)
+
+        score_text.text = 0.toString()
+
+        addOneButton.setOnClickListener {
+            score_text.text =  (score_text.text.toString().toInt() +1) .toString()
+
+        }
+    }
+}
